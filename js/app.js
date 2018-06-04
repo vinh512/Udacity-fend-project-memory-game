878cd260c57f0ac98da79cccc33734837b713c6c
Vinh512 <vvvhuynh@yahoo.com>
2018-06-03 23:08:56 -0500

    hides mismatching cards after delay

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
- let cardsNodeList = document.querySelectorAll(".card");
+let cardsNodeList = document.querySelectorAll(".card");
 
- let cardsArray = Array.from(cardsNodeList);
- console.log(cardsArray);
+let cardsArray = Array.from(cardsNodeList);
+console.log(cardsArray);
 
 /*
  * Display the cards on the page
@@ -13,34 +13,68 @@
  *   - add each card's HTML to the page
  */
 
- let currentCards = document.querySelectorAll(".card");
- currentCards = Array.from(currentCards);
+let currentCards = document.querySelectorAll(".card");
+currentCards = Array.from(currentCards);
 
- function flip(currentCards) {
-   currentCards.forEach(function(card){
-     card.addEventListener("click", function(){
-     card.classList.add("open", "show");
+let openCards = [];
+
+
+function flip(currentCards) {
+
+
+  currentCards.forEach(function(card) {
+    card.addEventListener("click", function() {
+
+      // ensures we're only able to click on blank cards
+      if (card.classList.length === 1) {
+
+        // only allows for 2 cards to be revealed
+        if (openCards.length < 2) {
+          openCards.push(card);
+          card.classList.add("open", "show");
+          console.log(openCards);
+          console.log("card child ", card.children[0].className);
+        }
+
+        // now that we have 2 cards selected, we check to see if they match otherwise re-hide them
+        if (openCards.length === 2) {
+          // checks to see if the cards are matching based on their font-awesome icon
+          if (openCards[0].children[0].className === openCards[1].children[0].className) {
+            console.log("Its a match!");
+          // if not matched, hides the cards again after 1sec delay
+          } else {
+            setTimeout(function() {
+              openCards.forEach(function(card) {
+                card.classList.remove("open", "show");
+              });
+              console.log("classes for non-matching cards have been removed")
+            }, 1000);
+          }
+        }
+      }
+
     });
-   });
- }
+  });
+}
 
 flip(currentCards);
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    console.log("cards shuffled");
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  console.log("cards shuffled");
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 
